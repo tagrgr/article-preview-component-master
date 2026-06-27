712c0f36ba4dfe74e1f8cadba097ca57e5d9dc6d
Tiago de Gregori <tiago.gregori@live.com>
2026-06-27 14:34:17 +0100

    responsive.css
    all mobile responsiveness ready and working properly

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,16 @@
 const shareButton = document.querySelector("#sharing-state");
 const toolTip = document.querySelector("#share-tooltip");
+const profileState = document.querySelector(".profile-state");
 
 shareButton.addEventListener("click", function() {
     toolTip.classList.toggle("hidden")
+
+    if(window.innerWidth <= 375) {
+        if(toolTip.classList.contains("hidden")) {
+            profileState.appendChild(shareButton)
+        } else {
+            toolTip.appendChild(shareButton)
+        }
+    }    
 })
+
